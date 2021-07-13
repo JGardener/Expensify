92602957e8a548e4f1a24861c0ff16b05b13e898
JGardener <jamesgardener92@gmail.com>
2021-07-13 10:25:42 +0100

    Replaced BrowserRouter >  div with <Switch> for adding a 404 route. Switch displays routes exclusively, so multiple routes won't display at once now!

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
@@ -11,16 +11,18 @@ const ExpenseDashboardPage = () => (<div>ExpenseDashboardPage</div>);
 const AddExpensePage = () => (<div>AddExpensePage</div>);
 const EditExpensePage = () => (<div>EditExpensePage</div>);
 const HelpPage = () => (<div>HelpPage</div>);
+const NotFoundPage = () => (<div>The page you were looking for was not found</div>)
 
 
 const routes = (
   <BrowserRouter>
-    <div>
+    <Switch>
       <Route path="/" component={ExpenseDashboardPage} exact={true} />
       <Route path="/create" component={AddExpensePage} />
       <Route path="/edit" component={EditExpensePage} />
       <Route path="/help" component={HelpPage} />
-    </div>
+      <Route component={NotFoundPage} />
+    </Switch>
   </BrowserRouter>
 );
 
@@ -44,11 +46,11 @@ To remedy this issue for when we only want AddExpensePage displayed, for example
 we use exact={true} to only show ExpenseDashboardPage if the URL matches
 exactly.
 
-What I've done:
+What I've done: Commit 3 - 12 Jul 21
 1. Installed React-Router-DOM (as opposed to React-Router-Native for mobiles)
 2. Imported BrowserRouter and Routes
 3. Down below, at line 16, I configured the routes for the application.
-  3.a BrowserRouter only needs to be initialised once.
+  3.a BrowserRouter only needs to be initialised once. It wants one child only. This is a div. After Commit 4, it is Switch
   3.b Route is initialised as many times as I have pages to route to.
 4. path and component are the URL and the component you display on the page when you visit that URL
 5. At this point, /create (as an example) would display both the ExpenseDashboardPage and AddExpensePage
@@ -59,4 +61,10 @@ What I've done:
   6.c Using historyApiFallback: true, I told the dev-server to fallback to the index.html file on all 404 errors.
 7. Profit!
 
+What I've done: Commit 4 - 12 Jul 21 - 13 Jul 21
+1. Replaced div with Switch - Switch renders routes exclusively.
+2. Added a route for when no page exists at that URL.
+3. This route doesn't contain a path, meaning it renders regardless of what route you use.
+4. Switch will look through all the routes until it finds something that matches. 
+5. The NotFoundPage will always match it, so it gets thrown up if nothing else matches!
 */
