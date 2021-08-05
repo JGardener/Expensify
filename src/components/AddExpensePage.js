f15648a21e6c9f41a88cfb3b2c0cb445d36f0ba4
JGardener <jamesgardener92@gmail.com>
2021-08-05 10:24:48 +0100

    Adding expenses directly. See comments

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import ExpenseForm from "./ExpenseForm";
 
 const AddExpensePage = () => (
-  <div>AddExpensePage</div>
-);
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
+)
 
 export default AddExpensePage;
