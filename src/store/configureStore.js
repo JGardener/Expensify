042ee2ff95bef5beefed230c1ea7f13ba134e957
JGardener <jamesgardener92@gmail.com>
2021-08-09 10:13:46 +0100

    Added the React Dev Tools Redux line that enables the extension to keep track of the redux store. Using this, I can track state and actions, making debugging problems much easier in the future, touch wood...

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,10 +5,11 @@ import filtersReducer from '../reducers/filters';
 // The first argument of createStore is combineReducers, because we want a reducer for each root state object. const store = createStore(
 export default () => {
     const store = createStore(
-    combineReducers({
-        expenses: expensesReducer,
-        filters: filtersReducer
-        })
+        combineReducers({
+            expenses: expensesReducer,
+            filters: filtersReducer
+        }),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
     return store;
 };
