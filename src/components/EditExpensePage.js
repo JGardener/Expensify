c84a2c45081edd0406981c64083f28b1065a5153
JGardener <jamesgardener92@gmail.com>
2021-10-19 09:59:20 +0100

    Added test cases for EditExpensePage

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -3,22 +3,58 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <ExpenseForm 
-      expense={props.expense}
-      onSubmit={(expense) => {
-        props.dispatch(editExpense(props.match.params.id, expense));
-        props.history.push('/');
-      }}
-      /> 
-      <button onClick={() => {props.dispatch(removeExpense({id: props.expense.id}));
-      props.history.push('/')}}>Remove</button>       
-    </div>
-  )
+// Refactor this to be class-based component. 
+// Map dispatch to props - we have editExpense and removeExpense
+
+// Three test cases
+
+// should render EditExpensePage
+// snapshot that. 
+
+// Should handle editExpense
+// spies
+
+// should handle removeExpense
+// spies
+
+// can use beforeEach to create the spies and then reuse them in each test
+export class EditExpensePage extends React.Component { 
+  onSubmit = (expense) => {
+    this.props.editExpense(this.props.expense.id, expense);
+    this.props.history.push('/');
+  }
+  onRemove = () => {
+    this.props.removeExpense({id: this.props.expense.id});
+    this.props.history.push('/');
+  }
+  render() {
+    return (
+      <div>
+        <ExpenseForm 
+        expense={this.props.expense}
+        onSubmit={this.onSubmit}
+        />
+        <button onClick={this.onRemove}>Remove</button>
+      </div>
+    )
+  }
 }
+
+// const EditExpensePage = (props) => {
+//   return (
+//     <div>
+//       <ExpenseForm 
+//       expense={props.expense}
+//       onSubmit={(expense) => {
+//         props.dispatch(editExpense(props.match.params.id, expense));
+//         props.history.push('/');
+//       }}
+//       /> 
+//       <button onClick={() => {props.dispatch(removeExpense({id: props.expense.id}));
+//       props.history.push('/')}}>Remove</button>       
+//     </div>
+//   )
+// }
 
 const mapStateToProps = (state, props) => {
   return {
@@ -26,4 +62,11 @@ const mapStateToProps = (state, props) => {
   }
 }
 
-export default connect(mapStateToProps)(EditExpensePage);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+    removeExpense: (data) => dispatch(removeExpense(data))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpensePage);
